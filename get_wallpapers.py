a915eef92178394e9e468a7e8d3fa8ef594179f3
Sreekesh Iyer <67017933+sreekeshiyer@users.noreply.github.com>
2021-11-24 14:51:40 +0530

    Update get_wallpapers.py

--- get_wallpapers.py
+++ get_wallpapers.py
@@ -1,7 +1,7 @@
 import requests
 from config import API_KEY as api_key
 
-
+## I've used landscapes and cityscapes in searches here, you could use whatever you want. Simply change the query="" in the URLs
 landscapes_url = 'https://api.unsplash.com/search/photos?page=1&query=landscape&per_page=20&order_by=latest&orientation=landscape&color=blue'
 cityscapes_url = 'https://api.unsplash.com/search/photos?page=1&query=cityscapes&per_page=20&orientation=landscape'
 headers = {
